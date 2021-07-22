25aee0d721e8bd1588e8f958271b858bdf815ba6
fairyhunter13 <fairyhunter13@gmail.com>
2021-07-22 09:17:21 +0700

    Finishing abstraction :sparkles:

--- oop/abstraction.go
+++ oop/abstraction.go
@@ -31,6 +31,56 @@ func (s *SMS) Send(destination, content string) {
 	fmt.Printf("Send sms to %s from %s, with text: %s.\n", s.destination, s.sender, s.text)
 }
 
-func abstraction() {
+type NotificationSender struct {
+	notifications map[string]Notification
+}
 
+type NotifParams struct {
+	Destination string
+	Content     string
+}
+
+func (np *NotifParams) IsValid() bool {
+	return np.Content != "" && np.Destination != ""
+}
+
+func (ns *NotificationSender) Send(medium string, params NotifParams) {
+	if !params.IsValid() {
+		return
+	}
+
+	notif, ok := ns.notifications[medium]
+	if !ok {
+		return
+	}
+
+	notif.Send(params.Destination, params.Content)
+}
+
+func abstraction() {
+	ns := NotificationSender{
+		notifications: map[string]Notification{
+			"email": &Email{
+				sender:  "me@mail.com",
+				subject: "no-reply",
+			},
+			"sms": &SMS{
+				sender: "0812109213123",
+			},
+		},
+	}
+
+	ns.Send("email", NotifParams{
+		Destination: "our@mail.com",
+		Content:     "This is an email",
+	})
+
+	ns.Send("sms", NotifParams{
+		Destination: "0811112223334",
+		Content:     "This is an sms",
+	})
+
+	ns.Send("email", NotifParams{
+		Content: "This doesn't get sent",
+	})
 }
