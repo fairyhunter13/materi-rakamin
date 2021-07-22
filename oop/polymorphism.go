9c2f4c3976c11c6e0f51df7b776f7894ac62a4b3
fairyhunter13 <fairyhunter13@gmail.com>
2021-07-22 15:47:17 +0700

    Finishing polymorphism :sparkles:

--- oop/polymorphism.go
+++ oop/polymorphism.go
@@ -1,10 +1,48 @@
 package main
 
+import "fmt"
+
 type Animal struct {
 	Name     string
+	Gender   string
 	IsMammal bool
 }
 
-func polymorphism() {
+func (a *Animal) Speak() {
+	var text string
 
+	text = fmt.Sprintf("I am %s. My gender is %s. I am not a mammal.", a.Name, a.Gender)
+	if a.IsMammal {
+		text = fmt.Sprintf("I am %s. My gender is %s. I am a mammal.", a.Name, a.Gender)
+	}
+	fmt.Println(text)
+}
+
+type Cat struct {
+	Animal
+	Breed string
+}
+
+func (a *Cat) Speak() {
+	a.Animal.Speak()
+	fmt.Printf("My breed is %s.\n", a.Breed)
+}
+
+func polymorphism() {
+	animalDiona := Animal{
+		Name:     "Diona",
+		Gender:   "female",
+		IsMammal: true,
+	}
+	animalDiona.Speak()
+
+	catKecing := Cat{
+		Animal: Animal{
+			Name:     "Kecing",
+			Gender:   "female",
+			IsMammal: true,
+		},
+		Breed: "Anggora",
+	}
+	catKecing.Speak()
 }
